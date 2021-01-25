f3b3b2ad8ffd2ab3374db6d0f7982324e7ab1803
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-25 12:09:59 +0000

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
 	get  '/signup',  to: 'users#new'
 	root   'static_pages#home'
   get    '/home',             to: 'static_pages#home'
+	get    '/login',            to: 'sessions#new'
+	post   '/login',            to: 'sessions#create'
+	delete '/logout',           to: 'sessions#destroy'
 	resources :users
 end
