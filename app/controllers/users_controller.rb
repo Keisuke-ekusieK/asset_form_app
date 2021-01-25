44c5dbb9af8c543a3d90f52ce004e153797d6909
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-25 11:05:05 +0000

    Add icon image to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+		#@monthly_expense = current_user.monthly_expenses.build if logged_in?
+		#@family = @user.family
+		#@asset_allocation = @user.asset_allocation
+		#if not @asset_allocation.nil?
+		#	@asset_allocation_for_graph = {'国内通貨預金' => @asset_allocation.domestic_currency_deposits,
+		#															 '海外通貨預金' => @asset_allocation.foreign_currency_deposits,
+		#															 '国内株式'     => @asset_allocation.domestic_stocks,
+		#															 '海外株式'    => @asset_allocation.foreign_stocks,
+		#															 '国内債券'    => @asset_allocation.domestic_bonds,
+		#															 '海外債券'    => @asset_allocation.foreign_bonds,
+		#															 '不動産'      => @asset_allocation.real_estate,
+		#															 '金'         => @asset_allocation.gold,
+		#															 '保険'       => @asset_allocation.insurance,
+		#															 'その他'     => @asset_allocation.others}
+		#end
+		#@monthly_expenses = @user.monthly_expenses.paginate(page: params[:page], per_page: 10)
+	end
+
   def new
 		@user = User.new
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_104446) do
+ActiveRecord::Schema.define(version: 2021_01_25_110356) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_104446) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
