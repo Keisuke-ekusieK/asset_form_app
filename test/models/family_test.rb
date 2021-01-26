b3160cd7b4068271d21c94a7c3d41d87fb8edab4
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-26 13:07:03 +0000

    Add test for family model

--- test/models/family_test.rb
+++ test/models/family_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class FamilyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+    @user = users(:michael)
+    @family = @user.build_family(spouse: true, children: 2, parent: 0, worker: 2, user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @family.valid?
+  end
+
+
+	###################
+	# test for :user_id
+	###################
+  test "user id should be present" do
+    @family.user_id = nil
+    assert_not @family.valid?
+  end
+
+	##################
+	# test for :spouse
+	##################
+	test "spouse column should be present" do
+		@family.spouse = nil
+		assert_not @family.valid?
+	end
+
+	####################
+	# test for :children
+	####################
+	test "children column should be present" do
+		@family.children = nil
+		assert_not @family.valid?
+	end
+
+	##################
+	# test for :parent
+	##################
+	test "parent column should be present" do
+		@family.parent = nil
+		assert_not @family.valid?
+	end
+
+	##################
+	# test for :worker
+	##################
+	test "worker column should be present" do
+		@family.worker = nil
+		assert_not @family.valid?
+	end
+
 end
