2d31f60adcaca6e96f6717ec113d407ba70b1f0a
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-26 13:01:53 +0000

    Add validates to family model

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,3 +1,18 @@
 class Family < ApplicationRecord
   belongs_to :user
+
+	validates :user_id,  presence: true
+	validates :spouse,   presence: true,
+											 inclusion: { in: [true, false] }
+	validates :children, presence: true,
+											 numericality: { only_integer: true,
+																			 greater_than_or_equal_to: 0,
+																			 less_than_or_equal_to: 20 }
+	validates :parent,   presence: true,
+											 numericality: { only_integer: true,
+																			 greater_than_or_equal_to: 0,
+																			 less_than_or_equal_to: 4 }
+	validates :worker,   presence: true,
+											 numericality: { only_integer: true,
+																			 greater_than_or_equal_to: 0 }
 end
