7791120a696ceb96e19f0f7708b56a3509bb8ace
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-26 23:53:52 +0000

    Add test for asset allocations controller

--- test/controllers/asset_allocations_controller_test.rb
+++ test/controllers/asset_allocations_controller_test.rb
@@ -2,18 +2,23 @@ require 'test_helper'
 
 class AssetAllocationsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get asset_allocations_new_url
+    get new_asset_allocation_url
     assert_response :success
   end
 
-  test "should get show" do
-    get asset_allocations_show_url
-    assert_response :success
+	test "should redirect create when not logged in" do
+    assert_no_difference 'AssetAllocation.count' do
+      post asset_allocation_path, params: { asset_allocation: { domestic_currency_deposits: 100,
+                                    														foreign_currency_deposts: 200,
+                                    														domestic_stocks: 1000,
+                                    														foreign_stocks: 1000,
+																																domestic_bonds: 100,
+																																foreign_bonds: 100,
+																																real_estate: 1000,
+																																gold: 100,
+																																insurance: 0,
+																																others: 100 } }
+    end
+    assert_redirected_to login_url
   end
-
-  test "should get edit" do
-    get asset_allocations_edit_url
-    assert_response :success
-  end
-
 end
