9c09c9a542857c2baf48daa42745c3d71cc64224
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-27 08:15:59 +0000

    Add relationships model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_032355) do
+ActiveRecord::Schema.define(version: 2021_01_27_081409) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -82,6 +82,16 @@ ActiveRecord::Schema.define(version: 2021_01_27_032355) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_monthly_expenses_on_user_id"
     t.index ["year", "month", "user_id"], name: "index_monthly_expenses_on_year_and_month_and_user_id", unique: true
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
