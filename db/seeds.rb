31c2a694b825ec2ac5b131f03a427c86c55449a4
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-27 13:36:49 +0000

    Set db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,18 @@ User.create!(name:  "Akari Keisuke",
              password_confirmation: "password",
 						 admin: true)
 
+User.create!(name:  "Example User",
+             email: "user@example.com",
+						 user_name: "example_user",
+             password:              "password",
+             password_confirmation: "password",
+						 admin: false)
+
 # 追加のユーザーをまとめて生成する
-99.times do |n|
+98.times do |n|
   name  = Faker::Name.name
-  email = "akari#{n+1}@example.com"
-	user_name = "example_#{n+1}"
+  email = "user#{n+2}@example.com"
+	user_name = "example_#{n+2}"
   password = "password"
   User.create!(name:  name,
                email: email,
@@ -21,61 +28,70 @@ end
 # 全ユーザーに家族構成の情報を追加
 users = User.order(:created_at).take(100)
 
-spouse = true
-children = 2
-parent = 0
-worker = 2
+spouse   = true
+children = [2,2,1,3,0,4]
+parent   = [0,0,0,0,0,0,1,1,2]
+worker   = [2,2,2,2,1,1,3]
+
+i = 0
 users.each { |user| 
 	user.build_family(spouse: spouse, 
-																			children: children,
-																			parent: parent,
-																			worker: worker) 
+																			children: children[i%6],
+																			parent: parent[i%9],
+																			worker: worker[i%7]) 
 	user.save
+	i += 1
 }
 
 # 全ユーザーに資産ポートフォリオを追加
-domestic_currency_deposits = 300
-foreign_currency_deposits = 100
-domestic_stocks = 100
-foreign_stocks = 200
-domestic_bonds = 0
-foreign_bonds = 0
-real_estate = 1000
-gold = 100
-insurance = 0
-others = 50
+domestic_currency_deposits = [100,200,300,400,500,600,1000,2000,5000]
+foreign_currency_deposits  = [0,0,0,0,100,500]
+domestic_stocks = [0,0,100,500,1000,2000,5000,3000,1000]
+foreign_stocks = [200,300,30,0,1000,500,300,200]
+domestic_bonds = [0,100,200,400,500,1000,0]
+foreign_bonds = [0,100,500,200]
+real_estate = [1000,500,2000,0,0]
+gold = [100,200,300]
+insurance = [0,0,0,0,300]
+others = [50,100,500,300,1000]
+
+j = 0
 users.each { |user| 
-	user.build_asset_allocation(domestic_currency_deposits: domestic_currency_deposits,
-																								foreign_currency_deposits: foreign_currency_deposits,
-																								domestic_stocks: domestic_stocks,
-																								foreign_stocks: foreign_stocks,
-																								domestic_bonds: domestic_bonds,
-																								foreign_bonds: foreign_bonds,
-																								real_estate: real_estate,
-																								gold: gold,
-																								insurance: insurance,
-																								others: others) 
+	user.build_asset_allocation(domestic_currency_deposits: domestic_currency_deposits[j%9],
+																								foreign_currency_deposits: foreign_currency_deposits[j%6],
+																								domestic_stocks: domestic_stocks[j%9],
+																								foreign_stocks: foreign_stocks[j%8],
+																								domestic_bonds: domestic_bonds[j%7],
+																								foreign_bonds: foreign_bonds[j%4],
+																								real_estate: real_estate[j%5],
+																								gold: gold[j%3],
+																								insurance: insurance[j%5],
+																								others: others[j%5]) 
 	user.save!
+	j += 1
 }
 
 # 最初の6人分だけ月の支出を登録する
-users = User.order(:created_at).take(6)
+users = User.order(:created_at).take(10)
 12.times do |n|
-	year = 2020
-	month = n+1
-	house = 40000 + 1000*n
-	food = 30000 - 100*n
-	clothes = 1000*n
-	education = 8000 - 200*n
-	daily_necessities = 3000 + n
-	car = 10000 - 100*n
-	medical_treatment = 2500 + n*100
-	entertainment = 300*n
-	communication = 2000 + 200*n
-	social = 5000 + 100*n
-	insurance = 200*n
-  comment = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.monthly_expenses.create!(year: year,
+	k = 0
+  users.each do |user| 
+		year = 2020
+		month = n+1
+		house = 50000 + 1000*n + 800*k
+		food = 30000 - 100*n -100*k
+		clothes = 1000*n
+		education = 8000 - 200*n
+		daily_necessities = 3000 + n +200*k
+		car = 10000 - 100*n -100*k
+		medical_treatment = 2500 + n*100
+		entertainment = 300*n
+		communication = 2000 + 200*n -10*k
+		social = 5000 + 100*n + 300*k
+		insurance = 200*n + 10*k
+  	comment = Faker::Lorem.sentence(word_count: 5)
+
+		user.monthly_expenses.create!(year: year,
 																										month: month,
 																										house: house,
 																										food: food,
@@ -88,7 +104,9 @@ users = User.order(:created_at).take(6)
 																										communication: communication,
 																										social: social,
 																										insurance: insurance,
-																										comment: comment) }
+																										comment: comment)
+		k += 1
+	end
 end
 
 # 以下のリレーションシップを作成する
