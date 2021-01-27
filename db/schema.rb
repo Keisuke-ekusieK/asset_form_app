9c3d632d1fc476090cb3bd584967436d997795bd
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-27 00:06:59 +0000

    Complete asset allocations model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_125733) do
+ActiveRecord::Schema.define(version: 2021_01_26_232745) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,23 @@ ActiveRecord::Schema.define(version: 2021_01_26_125733) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "asset_allocations", force: :cascade do |t|
+    t.integer "domestic_currency_deposits"
+    t.integer "foreign_currency_deposits"
+    t.integer "domestic_stocks"
+    t.integer "foreign_stocks"
+    t.integer "domestic_bonds"
+    t.integer "foreign_bonds"
+    t.integer "real_estate"
+    t.integer "gold"
+    t.integer "insurance"
+    t.integer "others"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_asset_allocations_on_user_id"
   end
 
   create_table "families", force: :cascade do |t|
@@ -58,5 +75,6 @@ ActiveRecord::Schema.define(version: 2021_01_26_125733) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "asset_allocations", "users"
   add_foreign_key "families", "users"
 end
