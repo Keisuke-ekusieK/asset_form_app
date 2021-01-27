fbbed08c3336a630b17f782bc5c8c29c0401eca8
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-27 08:41:32 +0000

    Add follower/followed path to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 	get    '/login',            to: 'sessions#new'
 	post   '/login',            to: 'sessions#create'
 	delete '/logout',           to: 'sessions#destroy'
-	resources :users
+	resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 	resource  :family,           only: [:new, :create, :edit, :update, :show]
 	resource  :asset_allocation, only: [:new, :create, :edit, :update, :show]
 	resources :monthly_expenses, only: [:create, :destroy]
