ca5f5833d63d9954348079f5c15faaab90a318ab
Keisuke-ekusieK <tennis.akari.abcdefg@gmail.com>
2021-01-27 08:18:31 +0000

    Add validations to relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
 	has_one  :family,           dependent: :destroy
 	has_one  :asset_allocation, dependent: :destroy
 	has_many :monthly_expenses, dependent: :destroy
+	has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+	has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
 
 	scope :search_by_keyword, -> (keyword) {
     where("users.name LIKE :keyword", keyword: "%#{sanitize_sql_like(keyword)}%") if keyword.present?
