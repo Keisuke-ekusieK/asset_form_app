d9010b3f8ed84deee84eb942de74c43dbc156d7c
Akari <68892662+Keisuke-ekusieK@users.noreply.github.com>
2021-01-29 19:07:28 +0900

    Update family model
    To register spouse column as false, presence validation should be false

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,7 +2,7 @@ class Family < ApplicationRecord
   belongs_to :user
 
 	validates :user_id,  presence: true
-	validates :spouse,   presence: true,
+	validates :spouse,   presence: false,
 											 inclusion: { in: [true, false] }
 	validates :children, presence: true,
 											 numericality: { only_integer: true,
